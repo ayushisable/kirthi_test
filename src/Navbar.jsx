c0fcb2f5ec0262a7539c3099dc37b9e3e3e7342b
ayushisable <subsable11@gmail.com>
2026-02-17 11:00:43 +0530

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.modules.css"
+import React from "react";
+const Navbar = ()=>{
+
+
+return(
+    <div>
+         <section className="parent">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Services</a>
+            <a href="">Contact</a>
+         </section>
+    </div>
+)
+
+}
+export default Navbar
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "../Navbar.module.css";
+
+const Home = () =>{
+  
+    
+  return (
+    <div>
+      
+      <section className={style.bg}>
+       <img src="" alt="" />
+       <h1> Welcome to home page</h1>
+
+      </section>
+    </div>
+  )
+
+}
